078a3b31ecc5bd8af2f0017447cd0336ea6f5831
Cocoze <colleen.sarah.zeller@hs-furtwangen.de>
2020-12-17 16:03:04 +0100

    aufgabe 9

--- Aufgabe9/ski.js
+++ Aufgabe9/ski.js
@@ -1,2 +1,17 @@
 "use strict";
+var Ski;
+(function (Ski) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        console.log("ski");
+        let canvas = document.querySelector("canvas");
+        if (!canvas)
+            return;
+        Ski.crc2 = canvas.getContext("2d");
+        Ski.crc2.fillStyle = "black";
+        Ski.crc2.strokeStyle = "white";
+        createPaths();
+        console.log("ski paths: ", skipaths);
+    }
+})(Ski || (Ski = {}));
 //# sourceMappingURL=ski.js.map
